98bca045782d8410929f02ba8028d8a2554cbc53
Yury Choe <yurychoe@gmail.com>
2021-08-25 21:09:31 -0400

    feat: popup input validation styles

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+// enableValidation({
+//   formSelector: '.popup__form',
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__save-button',
+//   inactiveButtonClass: 'button_inactive',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__input-error_active'
+// })
