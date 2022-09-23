90b13288fea67ea0579964b543eae2e0d9f474bb
Yury Choe <yurychoe@gmail.com>
2022-09-23 13:54:42 -0400

    feat: add likes counter and specify trash icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -32,6 +32,12 @@ export default class Card {
     this._element = null;
   }
 
+  _handleTrashIcon = () => {
+    if (this._data.owner._id != "5872314bc3b100b98081a62a") {
+      this._cardDeleteButton.remove();
+    }
+  }
+
   _handleName = () => {
     // checks and reassigns cards with "name"
     if (this._data.name) {
@@ -45,13 +51,17 @@ export default class Card {
     this._cardLikeButton = this._element.querySelector('.button_type_like');
     this._cardDeleteButton = this._element.querySelector('.button_type_delete');
     this._cardImage = this._element.querySelector('.photos__card-img');
+    this._likesCounter = this._element.querySelector('.reaction__count_likes');
     const cardTitle = this._element.querySelector('.photos__title');
+
+    this._handleTrashIcon();
     this._handleName();
 
     this._cardImage.src = this._data.link;
     this._cardImage.alt = this._data.title;
     cardTitle.textContent = this._data.title;
-    
+    this._likesCounter.textContent = this._data.likes.length;
+
     this._setEventListeners();
 
     return this._element;
